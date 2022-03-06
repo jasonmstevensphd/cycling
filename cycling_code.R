487f3ce79e50c0859fa9a1fa8a92a36aecb6a4f9
Jason Stevens <jasonmstevensphd@gmail.com>
2022-03-06 17:18:52 -0500

    Generated summary statistics and an initial plot

---
+++ cycling_code.R
@@ -0,0 +1,51 @@
+
+# Load Libraries ----------------------------------------------------------
+
+library(tidyverse)
+library(readxl)
+library(lubridate)
+
+# Import Files ------------------------------------------------------------
+
+cycling <- read_xlsx("C:/Users/jason/OneDrive/Documents/2022_CyclingLog (2).xlsx", sheet = "Cycling")
+
+
+
+# Data Wrangling ----------------------------------------------------------
+
+cycling$Week <- week(cycling$Date)
+cycling$Year <- year(cycling$Date)
+
+cycling_weekly <- cycling %>%
+  group_by(Year, Week, Bike, Ride) %>%
+  summarize(Avg_Time = mean(`Time...8`),
+            Avg_Calories = mean(Calories),
+            Avg_Distance = mean(Miles),
+            Avg_Temp = mean(Temp),
+            Avg_Humidity = mean(Humidity),
+            Avg_Wind = mean(Wind),
+            Total_Time = sum(`Time...8`),
+            Total_Calories = sum(Calories),
+            Total_Distance = sum(Miles))
+
+
+cycling_weekly$Week <- ifelse(cycling_weekly$Week %in% c(1:9), paste0(0, cycling_weekly$Week), cycling_weekly$Week)
+cycling_weekly$Entry <- paste0(cycling_weekly$Year, "_", cycling_weekly$Week)
+
+
+# Plot --------------------------------------------------------------------
+
+progress_plot <- ggplot(cycling_weekly, aes(x = Entry, y = Total_Time, group = Bike, fill = Bike))+
+  geom_bar(stat = "identity")+
+  theme(plot.background = element_rect(fill="white"))+
+  theme(panel.background = element_rect(fill="white", colour="grey50"))+
+  theme(plot.title = element_text(face = "bold", 
+                                  size = 18,
+                                  color="Navy"))+
+  theme(axis.title = element_text(face = "bold", size = 16))+
+  theme(axis.text.x = element_text(vjust = 1,
+                                   hjust = 1,
+                                   size = 12,
+                                   angle = 60))
+
+progress_plot
